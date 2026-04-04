aca52565dbd7734d06f534e5f00e0197930572cb
wodus331-afk <wodus331@novaair.kr>
2026-04-05 01:10:39 +0900

    vault backup: 2026-04-05 01:10:39

--- quartz/components/ProfileImage.tsx
+++ quartz/components/ProfileImage.tsx
@@ -1,22 +1,23 @@
-import {pathToRoot} from "../util/path"
-import {QuartzComponent, QuartzComponentConstructor, QuartzComponentProps} from "./types"
+import { pathToRoot } from "../util/path"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { classNames } from "../util/lang"
 
-const ProfileImage: QuartzComponent = ({fileData}: QuartzComponentProps) => {
-    const baseDir = pathToRoot(fileData.slug!)
-    return (
-        <div class="profile">
-            <img src={`${baseDir}/static/profile.webp`} alt="Profile"/>
-        </div>
-    )
+const ProfileImage: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
+  const baseDir = pathToRoot(fileData.slug!)
+  return (
+    <div class={classNames(displayClass, "profile")}>
+      <img src={`${baseDir}/static/profile.webp`} alt="Profile" />
+    </div>
+  )
 }
 
 ProfileImage.css = `
 .profile {
-    justify-content: center;
-    align-items: center;
-    margin-right: 10px;
-    display: flex;
-    transform: translate(-15px);
+  justify-content: center;
+  align-items: center;
+  margin-right: 10px;
+  display: flex;
+  transform: translate(-15px);
 }
 
 .profile img {
